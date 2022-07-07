703d4d26953de9afc1355e2e281c22ca096221d0
Kelishbekov <syrgakkelishbekov@gmail.com>
2022-07-07 09:54:01 +0300

    al

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import logo from "./logo.svg";
+import img from "./image1.svg";
 import "./App.css";
 
 function App() {
@@ -11,6 +11,22 @@ function App() {
             Some of the features and advantages that we provide for those of you
             who store data in this Data Warehouse.
           </p>
+        </div>
+        <div className="App__card">
+          <div className="Card__block">
+            <div className="Right__info">
+              <img src={img} />
+            </div>
+            <div className="Left__info">
+              <h3>Search Data</h3>
+              <p>
+                Don’t worry if your data is very large, the Data Warehoue
+                provides a search engine, which is useful for making it easier
+                to find data effectively saving time.
+              </p>
+              <a>Learn more</a>
+            </div>
+          </div>
         </div>
         <div></div>
       </div>
