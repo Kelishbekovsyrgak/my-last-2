b25040ff59c14d6ccf33c5b4f219849cb4aa858a
Kelishbekov <syrgakkelishbekov@gmail.com>
2022-07-07 10:37:45 +0300

    adap

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
-import img from "./image1.svg";
+import img1 from "./image1.svg";
+import img2 from "./image2.svg";
+import img3 from "./image3.svg";
+import img4 from "./image4.svg";
+import strelka from "./strelka.svg";
 import "./App.css";
 
 function App() {
@@ -15,7 +19,7 @@ function App() {
         <div className="App__card">
           <div className="Card__block">
             <div className="Right__info">
-              <img src={img} />
+              <img src={img1} />
             </div>
             <div className="Left__info">
               <h3>Search Data</h3>
@@ -24,7 +28,63 @@ function App() {
                 provides a search engine, which is useful for making it easier
                 to find data effectively saving time.
               </p>
-              <a>Learn more</a>
+              <div className="info__footer">
+                <a href="#">Learn more</a>
+                <img src={strelka} />
+              </div>
+            </div>
+          </div>
+
+          <div className="Card__block">
+            <div className="Right__info">
+              <img src={img2} />
+            </div>
+            <div className="Left__info">
+              <h3>24 Hours Access</h3>
+              <p>
+                Access is given 24 hours a full morning to night and meet again
+                in the morning, giving you comfort when you need data when
+                urgent.
+              </p>
+              <div className="info__footer">
+                <a href="#">Learn more</a>
+                <img src={strelka} />
+              </div>
+            </div>
+          </div>
+
+          <div className="Card__block">
+            <div className="Right__info">
+              <img src={img3} />
+            </div>
+            <div className="Left__info">
+              <h3>Print Out</h3>
+              <p>
+                Print out service gives you convenience if someday you need
+                print data, just edit it all and just print it..
+              </p>
+              <div className="info__footer">
+                <a href="#">Learn more</a>
+                <img src={strelka} />
+              </div>
+            </div>
+          </div>
+
+          <div className="Card__block">
+            <div className="Right__info">
+              <img src={img4} />
+            </div>
+            <div className="Left__info">
+              <h3>Security Code</h3>
+              <p>
+                Data Security is one of our best facilities. Allows for your
+                files to be safer. The file can be secured with a code or
+                password that you created, so only you can open the file.
+              </p>
+              <div className="info__footer">
+                <a href="#">Learn more</a>
+                <img src={strelka} />
+              </div>
             </div>
           </div>
         </div>
