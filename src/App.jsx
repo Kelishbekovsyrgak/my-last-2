f35d7c02d4adf02ec08193e1f4aa043b93053a1a
Kelishbekov <syrgakkelishbekov@gmail.com>
2022-07-07 09:36:22 +0300

    app title

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App__container">
+        <div className="App__title">
+          <h2>Features</h2>
+          <p>
+            Some of the features and advantages that we provide for those of you
+            who store data in this Data Warehouse.
+          </p>
+        </div>
+        <div></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
